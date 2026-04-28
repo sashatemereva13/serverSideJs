07790d0d01368a562d52a078931bb608a8384021
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-04-28 18:45:13 +0200

    fix: prevent invalid ObjectId crashes and handle not-found cases in course controller; enforce schema validation on updates

--- BACK/controllers/courseController.js
+++ BACK/controllers/courseController.js
@@ -1,26 +1,80 @@
 import * as service from "../services/courseService.js";
+import mongoose from "mongoose";
+import { BadRequestError, NotFoundError } from "../utils/errors.js";
 
-export const getCourses = async (req, res) => {
-  const data = await service.getCourses();
-  res.json(data);
+// get all courses
+export const getCourses = async (req, res, next) => {
+  try {
+    const data = await service.getCourses();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getCourse = async (req, res) => {
-  const data = await service.getCourseById(req.params.id);
-  res.json(data);
+// get one course
+export const getCourse = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestError("invalid course id");
+    }
+
+    const data = await service.getCourseById(id);
+
+    if (!data) {
+      throw new NotFoundError("course not found");
+    }
+
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const createCourse = async (req, res) => {
-  const data = await service.createCourse(req.body);
-  res.status(201).json(data);
+export const createCourse = async (req, res, next) => {
+  try {
+    const data = await service.createCourse(req.body);
+    res.status(201).json(data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const updateCourse = async (req, res) => {
-  const data = await service.updateCourse(req.params.id, req.body);
-  res.json(data);
+export const updateCourse = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestError("invalid course id");
+    }
+    const data = await service.updateCourse(id, req.body);
+
+    if (!data) {
+      throw new NotFoundError("course not found");
+    }
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const deleteCourse = async (req, res) => {
-  await service.deleteCourse(req.params.id);
-  res.status(204).end();
+export const deleteCourse = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestError("invalid course id");
+    }
+
+    const data = await service.deleteCourse(id);
+
+    if (!data) {
+      throw new NotFoundError("course not found");
+    }
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 };
--- BACK/services/courseService.js
+++ BACK/services/courseService.js
@@ -1,8 +1,11 @@
 import Courses from "../models/Courses.js";
 
-export const getCourses = () => Courses.find();
-export const getCourseById = (id) => Courses.findById(id);
+export const getCourses = () => Courses.find().lean();
+export const getCourseById = (id) => Courses.findById(id).lean();
+
 export const createCourse = (data) => Courses.create(data);
+
 export const updateCourse = (id, data) =>
-  Courses.findByIdAndUpdate(id, data, { new: true });
+  Courses.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+
 export const deleteCourse = (id) => Courses.findByIdAndDelete(id);
