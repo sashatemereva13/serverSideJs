3da96d325c0206c60395c114ca5efbf4da56d5c4
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-04-28 18:01:31 +0200

    refactor: use standard JWT 'sub' claim for user id

--- BACK/utils/jwt.js
+++ BACK/utils/jwt.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
+  return jwt.sign(
+    { sub: user._id.toString(), role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
 };
