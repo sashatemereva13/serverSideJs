1e37d67fc47b25d37a6db89f1bbd0661df89255d
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-04-28 17:57:22 +0200

    refactor: add safe JSON parsing in cache utility

--- BACK/utils/cache.js
+++ BACK/utils/cache.js
@@ -1,8 +1,12 @@
 import redisClient from "../config/redis.js";
 
 export const getCache = async (key) => {
-  const data = await redisClient.get(key);
-  return data ? JSON.parse(data) : null;
+  try {
+    const data = await redisClient.get(key);
+    return data ? JSON.parse(data) : null;
+  } catch {
+    return null;
+  }
 };
 
 export const setCache = async (key, value, ttl = 60) => {
